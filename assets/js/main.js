85ea48aff69bcc979bc4ae525ddbae04b6a4fadc
aemeny <alexemeny73@gmail.com>
2024-09-18 17:03:40 +0100

    Icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,6 +71,105 @@
 	});
 
 
+	//PARTICLES
+	particlesJS("particles-js", {
+		"particles": {
+		"number": {
+			"value": 50, // Number of particles
+		},
+		"color": {
+			"value": "#85C1E9"  // Particle color
+		},
+		"shape": {
+			"type": "circle",
+			"stroke": {
+			"width": 0,
+			"color": "#85C1E9"
+			},
+		},
+		"opacity": {
+			"value": 0.5,
+			"random": false,
+			"anim": {
+			"enable": false,
+			"speed": 1,
+			"opacity_min": 0.1,
+			"sync": false
+			}
+		},
+		"size": {
+			"value": 3,
+			"random": true,
+			"anim": {
+			"enable": false,
+			"speed": 40,
+			"size_min": 0.1,
+			"sync": false
+			}
+		},
+		"line_linked": {
+			"enable": true,
+			"distance": 150,
+			"color": "#85C1E9",
+			"opacity": 0.8,
+			"width": 1
+		},
+		"move": {
+			"enable": true,
+			"speed": 1.1, // Slow down particle movement
+			"direction": "none",
+			"random": false,
+			"straight": false,
+			"out_mode": "out",
+			"bounce": false,
+			"attract": {
+			"enable": false,
+			"rotateX": 600,
+			"rotateY": 1200
+			}
+		}
+		},
+		"interactivity": {
+		"detect_on": "canvas",
+		"events": {
+			"onhover": {
+			"enable": false,
+			},
+			"onclick": {
+			"enable": false,
+			},
+			"resize": true
+		},
+		"modes": {
+			"grab": {
+			"distance": 200,  // Distance at which particles grab to the mouse
+			"line_linked": {
+				"opacity": 0.8  // Opacity of lines connected to mouse
+			}
+			},
+			"bubble": {
+			"distance": 400,
+			"size": 40,
+			"duration": 2,
+			"opacity": 8,
+			"speed": 3
+			},
+			"repulse": {
+			"distance": 200,
+			"duration": 0.4
+			},
+			"push": {
+			"particles_nb": 4
+			},
+			"remove": {
+			"particles_nb": 2
+			}
+		}
+		},
+		"retina_detect": true
+	});
+
+
 	// SLIDESHOW
 	let slideIndex = 0;
     showSlides();
